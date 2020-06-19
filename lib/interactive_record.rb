b2df52e79cb9d31a28a1ad181d9bf536a8af577a
Joseph Kao <josephtkao@gmail.com>
2020-06-19 18:25:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,5 +50,8 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() from #{self.table_name_for_insert}")[0][0]
   end
 
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name =", name)
+  end
 
 end
